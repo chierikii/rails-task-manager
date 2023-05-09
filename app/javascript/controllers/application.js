7ab353e8160dfdc619f3622ca07733cfeb64625f
Chieri Kii <kiichieri@gmail.com>
2023-05-09 15:09:20 +0900

    update

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,3 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
